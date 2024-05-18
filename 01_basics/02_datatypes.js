b718fbfdc3065b1065a84c2eede1f68591c07fdf
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-05-18 06:26:21 +0000

    learn datatypes js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+    console.log("Avinash")
+
+    let name = "Avinash" //String Data types
+    let age = 33 // number datatypes
+    let isLoggedIn = false // Boolean datatypes
+
+    // number => 2 to power 53
+    // bigint => greater than 2 to power 53 end with n
+    // string => "" or ''
+    // boolean => true/false
+    // null => standalone  value
+    // undefined => not assigned any value yet
+    // symbol =>  unique (defined uniqueness of any variable)
+
+    // object =>  
+        console.log(typeof undefined); // undefined
+        console.log(typeof null); // object
