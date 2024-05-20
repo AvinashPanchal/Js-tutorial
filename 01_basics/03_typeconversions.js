e1b5a2b6a832da6fa015e470c85b645412131d2d
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-05-20 06:03:46 +0000

    datatypes conversions

---
+++ 01_basics/03_typeconversions.js
@@ -0,0 +1,55 @@
+//let score = 33
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// let score = "33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
+
+//let score = null;
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //0
+
+//let score = undefined;
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //Nan
+
+//let score = true;
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //1
+
+//let score = "Avinash";
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //1
+
+//let isLoggedIn = 1;
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(typeof booleanIsLoggedIn);//boolean
+//console.log(booleanIsLoggedIn);//true
+
+//let someNumber = 33;
+//let stringNumber = String(someNumber);
+//console.log(typeof stringNumber); // string
+//console.log(stringNumber); // 33
+
+let score = "";
+let scoreNumber = Number(score);
+console.log(typeof scoreNumber); // number
+console.log(scoreNumber); // 0
+
