86b6308e1bc9ec633a26ca2e832f81888c83ca1e
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-05-21 05:47:46 +0000

    Js Comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1); //true
+// console.log(2>=1); //true
+// console.log(2<1); //false
+// console.log(2==1); //false
+// console.log(2!=1); //true
+
+// console.log("2">1); //true  Here JS automatically converted string to number while comparioning
+// console.log("02">1); //true 
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true Here JS conveted null to 0 then compare
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+// "====" compare both datatypes as well as value.
+
+console.log("2"=== 2);//false
