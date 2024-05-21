3990d48ff714f1cd6eae179d51ab9cb20beb2003
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-05-21 05:27:43 +0000

    JS Operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,32 @@
+let value=3
+let negativeValue = -value
+//console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = " Avinash";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log ("1"+2); //12
+// console.log (1+"2"); //12
+// console.log ("1"+"2");//12
+
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32 converted according to the first datatypes
+
+// console.log(+true);//1
+// console.log(true+);// unxepected error
+// console.log(+""); //0
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
