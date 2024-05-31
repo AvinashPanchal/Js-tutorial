a4a273085adeb49d3025745154d9de67650a1aef
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-05-31 05:26:10 +0000

    made dev changes

--- 01_basics/09_numbermath.js
+++ 01_basics/09_numbermath.js
@@ -2,18 +2,19 @@
 // console.log(score);
 
 
-const balance = new Number(100)
+// const balance = new Number(100)
 // console.log(balance); // [Number: 100]
 
-console.log(balance.toString()); //100
-console.log(balance.toString().length); //3
+// console.log(balance.toString()); //100
+// console.log(balance.toString().length); //3
 
-console.log(balance.toFixed(2)); // 100.00
-
-
-const otherNumber = 123.8966
-
-console.log(otherNumber.toPrecision(3)); //124
-console.log(otherNumber.toPrecision(4)); //123.9
+// console.log(balance.toFixed(2)); // 100.00
 
 
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3)); //124
+// console.log(otherNumber.toPrecision(4)); //123.9
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
