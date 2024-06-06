29e1c08da3a8d92407987a0d3fffb5607f79f2e0
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-06-06 07:48:28 +0000

    object and json data

---
+++ 02_basics/05_object.js
@@ -0,0 +1,33 @@
+// const course = {
+//     coursename: "JS Hindi",
+//     price: "999",
+//     courseinstructor: "Avinash"
+// }
+
+// const {courseinstructor} = course
+// console.log(courseinstructor); // Avinash
+
+//console.log(course.courseinstructor); //Avinash
+
+//object destructuring
+const {courseinstructor:instructor} = course
+console.log(instructor); //Avinash
+
+
+// JSON Format
+// {
+//     "name":"Avinash",
+//     "coursename": "JS Hindi",
+//     "price": "free"
+    
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+// tools like json fomatter are used for 
+
+
