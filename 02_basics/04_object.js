fd4dd0c4520ee207040d7f9af893bac098474a24
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-06-06 06:38:11 +0000

    JS Object more

---
+++ 02_basics/04_object.js
@@ -0,0 +1,11 @@
+// const tinderUser = new Object() // constructor
+// console.log(tinderUser); // {}
+
+const tinderUser = {} // non singleton or literals
+tinderUser.id = "123abc"
+tinderUser.name = "Amit"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser); // {} 
+
+
