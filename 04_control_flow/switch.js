de4b76b57361f23da7959676d08c58bfceaa13ad
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-06-12 07:55:00 +0000

    switch case js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+// Switch Case  (shift+alt+down arrow for duplicate in vs code)
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Break - whrere key match then after break all code not executed except default
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March"); // March
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Default Case");
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March"); // March
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case");
+        break;
+}
+
