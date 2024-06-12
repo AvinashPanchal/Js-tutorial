33da81e748a7501fdc484cfb67d73ef33f4a9f01
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-06-12 09:45:34 +0000

    Nullish Coalescing Operator

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,58 @@
+//falsy Value - false, 0, -0, BigInt 0n, "", null, undefind, NaN
+//truethy value - all not falsy, "0", 'false', " "(space), [], {}, function(){} 
+
+// const userEmail = "avinash@ai.com"
+
+// if (userEmail) {
+//     console.log("Got User Email"); //Got User Email
+// }else{
+//     console.log("Don't Have User Mail");
+// }
+
+// const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got User Email"); // Got User Email
+// }else{
+//     console.log("Don't Have User Mail");
+// }
+
+// const userEmail = []
+
+// if (userEmail.length===0) {
+//     console.log("Array is empty"); // Array is empty
+// }else{
+//     console.log("Don't Have User Mail");
+// }
+
+// const emptyObject={}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Empty Object"); //Empty Object
+// }
+
+// false==0 true
+//false=='' true
+//0 == '' true
+
+// Nullish Coalescing Operator (??): null, undefind
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+//Ternery Operator 
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80")
+
+
+
+
