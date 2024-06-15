2808dc1dc67690d94becd48c9c7347f0ea9914fe
Avinash Kumar <er.avinashpanchal@gmail.com>
2024-06-15 09:51:23 +0000

    map and filters

---
+++ 05_iterations/hol4.js
@@ -0,0 +1,18 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.map( (num)=> num+10)
+// console.log(newNums);
+
+// const newNums = myNums.map( (num)=> {num+10} )
+// console.log(newNums);
+
+// in {} scope we get undefined until the we not specify return statement
+
+//const newNums = myNums.map( (num)=> {return num+10} )
+//Chaining Map
+const newNums = myNums
+                .map((num)=>num*10)
+                .map((num)=> num+1)
+                .filter((num)=> num>=40)
+console.log(newNums);
+
